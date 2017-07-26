cbf1221800a5260373c366013e52855d49f78f48
alvich <alvichm@gmail.com>
2017-07-26 09:07:08 -0400

    scripting obstaculos y mesh render
    mesh render etc script IA de los obstaculos, NO OLVIDAR CARVE only stationary

---
+++ Assets/Scripts/Mis scripts/enemigocosa1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;  
+
+public class enemigocosa1 : MonoBehaviour {
+    public NavMeshAgent agente;
+    public Transform destino;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agente.SetDestination(destino.position);
+
+		
+	}
+}
